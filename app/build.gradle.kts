55d60f608e174ce6565cae8030df715968ad0b98
ali33035528yad-afk <ali33035528yad@gmail.com>
2026-09-10 19:36:04 +0330

    Move Kotlin compilerOptions to Kotlin extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,13 @@ android {
         }
     }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+    buildFeatures { compose = true; buildConfig = true }
+}
+
+kotlin {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
-    buildFeatures { compose = true; buildConfig = true }
 }
 
 dependencies {
